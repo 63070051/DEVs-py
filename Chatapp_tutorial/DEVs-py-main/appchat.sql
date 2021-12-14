5442408e584db0268cd55473e73b02c7caf61b8e
it63070101 <73680308+it63070101@users.noreply.github.com>
2021-12-14 19:49:23 +0700

    Add files via upload

---
+++ Chatapp_tutorial/DEVs-py-main/appchat.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 22, 2021 at 12:42 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `appchat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `firstname` varchar(100) DEFAULT NULL,
+  `lastname` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `card_id` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `number_friend` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Chatapp_tutorial/DEVs-py-main/backend.py
@@ -0,0 +1,155 @@
+# import pymysql
+import pymysql
+from flask import Flask, render_template, request, jsonify, make_response, session, redirect, url_for
+from flask_socketio import SocketIO, join_room, leave_room, emit
+
+app = Flask(__name__)
+conn = pymysql.connect(host="localhost", user="root", password="", db="appchat")
+app.secret_key = 'any random string'
+app.debug = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+socketio = SocketIO(app)
+
+@app.route("/")
+def main():
+    """for render login page"""
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    """go to home"""
+    return render_template("home.html", num_people = int(session['number_friend']), myId=session['my_id'])
+
+@app.route("/register_page")
+def register_page():
+    """go to register"""
+    return render_template("register.html")
+
+
+@app.route("/login_page")
+def login_page():
+    """go to login"""
+    return render_template("index.html")
+
+
+# login and register system to sql
+@app.route("/register_data", methods=["POST"])
+def register_data():
+    """register"""
+    if request.method == "POST":
+        with conn.cursor() as cursor:
+            email = request.form["email"]
+            firstname = request.form["firstname"]
+            lastname = request.form["lastname"]
+            password = request.form["password"]
+            card_id = request.form["card_id"]
+            sql = "SELECT email FROM users WHERE email = %s"
+            cursor.execute(sql, email)
+            account = cursor.fetchone()
+            if(account):
+                msg = '* this email is have in system'
+                return render_template("register.html", msg=msg)
+            else :
+                sql = "Insert into `users` (`firstname`, `lastname`, `email`, `password`, `number_friend`, `card_id`) values(%s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (firstname, lastname, email, password, 0, card_id))
+                conn.commit()
+                return render_template("index.html")
+    return render_template("register.html")
+
+
+@app.route("/login_data", methods=["POST"])
+def login_data():
+    """login"""
+    if (
+        request.method == "POST"
+        and "email" in request.form
+        and "password" in request.form
+    ):
+        with conn.cursor() as cursor:
+            email = request.form["email"]
+            password = request.form["password"]
+            sql = "SELECT * FROM users WHERE email = %s AND password = %s"
+            cursor.execute(sql, (email, password))
+            account = cursor.fetchone()
+            print(email, password)
+            print(account)
+            if account:
+                session['loggedin'] = True
+                session['id'] = account[0]
+                session['number_friend']=account[6]
+                session['my_id'] = account[4]
+                print(session['number_friend'])
+                print("login success")
+                print(session)
+                return render_template("home.html", num_people=int(session['number_friend']), myId=session['my_id'])
+            else:
+                msg = "* not have a email in system"
+                return render_template("index.html", msg=msg)
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    print(session)
+    session.pop("loggedin", None)
+    session.pop("id", None)
+    return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    with conn.cursor() as cursor:
+        id = session['id']
+        sql = "SELECT * FROM users WHERE id=%s"
+        cursor.execute(sql, id)
+        account = cursor.fetchone()
+        print(id)
+        print(account)
+        if account:
+            return render_template("profile.html", firstname=account[1], lastname=account[2], email=account[3], password=account[5], myId=session["my_id"])
+        else:
+            return render_template("notfound")
+
+# @app.route("/addFriend", methods=["POST"])
+# def addfriend():
+#     """for add friend"""
+#     id_friend = request.form["data"]
+#     if request.method == "POST":
+#         print(id_friend)
+#         with conn.cursor() as cursor:
+#             sql = "INSERT INTO users(weight, desiredWeight) SELECT weight, desiredWeight FROM AnotherTable WHERE id = %s"
+#             cursor.execute(sql, (firstname, lastname, email, password, 0, card_id))
+#             conn.commit()
+#     return render_template("home_onAddFriend.html")
+
+# @app.route('/chat', methods=['GET', 'POST'])
+# def chat():
+#     if(request.method=='POST'):
+#         username = request.form['username']
+#         room = request.form['room']
+#         #Store the data in session
+#         session['username'] = username
+#         session['room'] = room
+#         return render_template('chat.html', session = session)
+#     else:
+#         if(session.get('username') is not None):
+#             return render_template('home.html', session = session)
+#         else:
+#             return redirect(url_for('index'))
+
+@app.route('/')
+def hello():
+  return render_template('./home.html')
+
+def messageRecived():
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('recived my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageRecived)
+
+if __name__ == '__main__':
+    socketio.run(app, debug = True)
